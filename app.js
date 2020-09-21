8dccec97e96b2a546596a0b3df98436b36e5a9f3
namtndus <rnrmf0721@gmail.com>
2020-09-21 17:19:56 +0900

    start to make the control

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router"
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 const app = express();
-
-const handleHone = (req, res) => res.send("hello from home")
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -16,11 +14,8 @@ app.use(bodyParser.urlencoded({ extendend: true }));
 app.use(helmet());
 app.use(morgan("tiny"));
 
-
-app.get("/", handleHone);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("videos", videoRouter);
 
 export default app;
