df885c7b58bd7bad4e3896b65209ca626c5bfab6
namtndus <rnrmf0721@gmail.com>
2020-09-21 15:27:31 +0900

    make init.js

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listeninig on: http://localhost:${PORT}`);
 const handleHone = (req, res) => res.send("hello from home")
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-app.use(morgan("tiny"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extendend: true }));
 app.use(helmet());
+app.use(morgan("tiny"));
+
 
 app.get("/", handleHone);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+export default app;
