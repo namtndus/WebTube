eedc4e907a0a800862cf099ec9d7bc5d274182cd
namtndus <rnrmf0721@gmail.com>
2020-09-25 10:12:03 +0900

    add search to search function in videoController

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,9 @@
 export const home = (req, res) => res.render("home", { siteTitle: "Home" });
-export const search = (req, res) => res.render("serch", { searchTitle: "Search" });
+export const search = (req, res) => {
+    const { query: { term: searchingBy } } = req;
+    console.log(res.locals);
+    res.render("serch", { searchTitle: "Search", searchingBy });
+}
 export const videos = (req, res) => res.render("videos", { videosTitle: "Videos" });
 export const upload = (req, res) => res.render("upload", { uploadTitle: "upload" });
 export const videoDetail = (req, res) => res.render("video Detail", {});
--- localsMiddleware.js
+++ localsMiddleware.js
@@ -1,4 +1,6 @@
+import routes from "./routes"
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";
+    res.locals.routes = routes;
     next();
 }
